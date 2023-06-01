b1f1f7e1726b6ed22ce95569a4490b0805c5f3af
Adberto Melo <adbertomelo@gmail.com>
2023-06-01 11:49:40 -0300

    adicionadas as rotas

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -1,1 +1,32 @@
-console.log("Hello World!")
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom';
+
+import Login from '../pages/account/login';
+import CreateUser from '../pages/user/create';
+import EditUser from '../pages/user/edit';
+import DeleteUser from '../pages/user/delete';
+import Admin from '../pages/admin'
+import ListOfUsers from '../pages/admin/listofusers'
+import Home from '../pages/home'
+
+export default function MainRoutes() {
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route exact path="/home" element={<Home />} />
+                <Route exact path="/user/create" element={<CreateUser />} />
+                <Route exact path="/user/edit/:id" element={<EditUser />} />
+                <Route exact path="/user/delete/:id" element={<DeleteUser />} />
+                <Route exact path="/admin" element={<Admin />} />
+                <Route exact path="/admin/listofusers" element={<ListOfUsers />} />
+            </Routes>
+        </BrowserRouter>
+
+    )
+};
