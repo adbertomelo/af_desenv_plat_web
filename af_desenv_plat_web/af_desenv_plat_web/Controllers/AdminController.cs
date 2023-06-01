d72fefbba137ec83344426878fabbe2e3892a69b
Adberto Melo <adbertomelo@gmail.com>
2023-06-01 08:37:47 -0300

    Implementação do controller admin

---
+++ af_desenv_plat_web/af_desenv_plat_web/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public AdminController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<List<User>> GetUsers() =>
+            await _userService.GetAsync();
+    }
+}
