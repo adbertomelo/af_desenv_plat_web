22f0140e7d24210522309f7e74b739cb8c579371
Adberto Melo <adbertomelo@gmail.com>
2023-05-31 21:46:26 -0300

    Criação da classe de serviços com métodos padrões

--- af_desenv_plat_web/af_desenv_plat_web/UserService.cs
+++ af_desenv_plat_web/af_desenv_plat_web/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using backend.Models;
+using Microsoft.Extensions.Options;
 
 namespace backend
 {
@@ -7,6 +8,30 @@ namespace backend
     {
 
         private readonly IMongoCollection<User> _userCollection;
+
+        public UserService(IOptions<DatabaseSettings> databaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                databaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                databaseSettings.Value.DatabaseName);
+
+            _userCollection = mongoDatabase.GetCollection<User>(
+                databaseSettings.Value.UsersCollectionName);
+        }
+
+        public async Task<List<User>> GetAsync() =>
+            await _userCollection.Find(_ => true).ToListAsync();
+
+        public async Task<User?> GetAsync(string id) =>
+            await _userCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task<User?> GetByLoginAsync(string email) =>
+            await _userCollection.Find(x => x.Email == email).FirstOrDefaultAsync();
+
+        public User? GetByLogin(string? email) =>
+            _userCollection.Find(x => x.Email == email).FirstOrDefault();
 
 
     }
