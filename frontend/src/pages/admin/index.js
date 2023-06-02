bbb7296478cebc5b6de54f54f4c70661bfcb608b
Adberto Melo <adbertomelo@gmail.com>
2023-06-02 05:16:51 -0300

    implementação da lista de usuários

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,1 +1,83 @@
-console.log("Hello World!")
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { URL_API } from '../../api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+const Admin = () => {
+
+    const [users, setUsers] = useState([]);
+    const [msgError, setMsgError] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        setMsgError('');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        fetch(`${URL_API}/admin/getusers`, requestOptions)
+            .then(response => {
+
+                if (!response.ok) {
+                    return Promise.reject(response.text());
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                setUsers(data);
+            })
+            .catch(error => {
+                error.then(
+                    msg => { setMsgError(msg) });
+            });
+
+    }, []);
+
+    const listOfUsers = () => {
+
+        const res = users.map((u) => {
+            return <tr key={u.id}>
+                <td>{u.name}</td>
+                <td>{u.email}</td>
+                <td><Link to={`/user/edit/${u.id}`} title="Alterar" ><FontAwesomeIcon icon={faPencil} /></Link></td>
+                <td><Link to={`/user/delete/${u.id}`} title="Excluir"><FontAwesomeIcon icon={faTrash} /></Link></td>
+            </tr>
+        });
+
+        return res;
+
+    }
+    return (
+
+
+        <div>
+            <h3>Usuários</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th colSpan="2">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        listOfUsers()
+                    }
+                </tbody>
+            </table>
+        </div>
+
+
+    );
+
+};
+
+export default Admin;
