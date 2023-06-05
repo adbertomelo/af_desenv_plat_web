edb14662ee19333bf25282dd366599c33608466f
Adberto Melo <adbertomelo@gmail.com>
2023-06-05 19:15:54 -0300

    ajustes nas rotas

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Header = ({ withHome = "true" }) => {
 
@@ -7,7 +7,7 @@ const Header = ({ withHome = "true" }) => {
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                {withHome == "true" ? <a class="navbar-brand" href="/home">Bolão10</a> : <p style={{ color: "white", fontSize:"var(--bs-navbar-brand-font-size)"} }>Bolão10</p> }                
+                {withHome == "true" ? <Link className="navbar-brand" to={'/home'}>Bolão10</Link> : <p style={{ color: "white", fontSize:"var(--bs-navbar-brand-font-size)"} }>Bolão10</p> }                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- frontend/src/pages/account/login.js
+++ frontend/src/pages/account/login.js
@@ -1,5 +1,5 @@
 ﻿import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { URL_API } from '../../api';
 import Header from '../../components/header'
 
